141474de551e1c6db060338e7560c46c3f900f88
Jonathan Helmus <jjhelmus@gmail.com>
2016-09-19 14:10:11 -0500

    Fix OS X whl URLS

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,9 @@
 
 if [ `uname` == Darwin ]; then
     if [ "$PY_VER" == "2.7" ]; then
-        pip install --no-deps https://storage.googleapis.com/tensorflow/mac/tensorflow-0.10.0-py2-none-any.whl
+        pip install --no-deps https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.10.0-py2-none-any.whl
     else
-        pip install --no-deps https://storage.googleapis.com/tensorflow/mac/tensorflow-0.10.0-py3-none-any.whl
+        pip install --no-deps https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.10.0-py3-none-any.whl
     fi
 fi
 
