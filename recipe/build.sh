4f544b1733e4af038b9684cc1957d7321d3363c6
Jonathan Helmus <jjhelmus@gmail.com>
2017-02-12 19:19:50 -0600

    Install from googleapis wheels on OS X
    Wheels from PyPI are pinned to the exact OS X version number which
    prevents installation on Travis CI.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,9 @@
 
 if [ `uname` == Darwin ]; then
     if [ "$PY_VER" == "2.7" ]; then
-        WHL_FILE=https://pypi.org/packages/cp27/t/tensorflow/tensorflow-${PKG_VERSION}-cp27-cp27m-macosx_10_11_x86_64.whl
-    elif [ "$PY_VER" == "3.5" ]; then
-        WHL_FILE=https://pypi.org/packages/cp35/t/tensorflow/tensorflow-${PKG_VERSION}-cp35-cp35m-macosx_10_11_x86_64.whl 
-    elif [ "$PY_VER" == "3.6" ]; then
-        WHL_FILE=https://pypi.org/packages/cp36/t/tensorflow/tensorflow-${PKG_VERSION}-cp36-cp36m-macosx_10_11_x86_64.whl 
+        WHL_FILE=https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-${PKG_VERSION}-py2-none-any.whl
+    else
+        WHL_FILE=https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-${PKG_VERSION}-py3-none-any.whl
     fi
 fi
 
