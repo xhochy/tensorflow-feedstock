914fd8426233b2395259127109f69175d298deb0
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2019-02-20 11:23:13 -0800

    Fix bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 IF "%PY_VER%"=="3.6" (
 	%PYTHON% -m pip install --no-deps https://pypi.org/packages/cp36/t/tensorflow/tensorflow-%PKG_VERSION%-cp36-cp36m-win_amd64.whl
 )
-IF "%PY_VER%"=="3.5" (
+IF "%PY_VER%"=="3.7" (
 	%PYTHON% -m pip install --no-deps https://pypi.org/packages/cp37/t/tensorflow/tensorflow-%PKG_VERSION%-cp37-cp37m-win_amd64.whl
 )
 if errorlevel 1 exit 1
