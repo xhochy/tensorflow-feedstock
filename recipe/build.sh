c30414a9e73baa5b0bfa128a042af46761fff68f
Isuru Fernando <isuruf@gmail.com>
2019-08-15 15:48:47 +0000

    Add osx support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 set -ex
+
+if [[ "$target_platform" == "osx-64" ]]; then
+  export CC=clang
+  export CXX=clang++
+fi
+
 export PATH="$PWD:$PATH"
 export CC=$(basename $CC)
 export CXX=$(basename $CXX)
@@ -11,30 +17,28 @@ echo "#!/bin/bash"                                > compiler-wrapper
 echo "export C_INCLUDE_PATH=$PREFIX/include"     >> compiler-wrapper
 echo "export CPLUS_INCLUDE_PATH=$PREFIX/include" >> compiler-wrapper
 chmod +x "compiler-wrapper"
-
 cp compiler-wrapper $CC
 cp compiler-wrapper $CXX
 echo "exec $BUILD_PREFIX/bin/$CC -L$PREFIX/lib \"\$@\""   >> $CC
 echo "exec $BUILD_PREFIX/bin/$CXX -L$PREFIX/lib \"\$@\""  >> $CXX
 
-export TF_SYSTEM_LIBS="llvm"
+export TF_SYSTEM_LIBS="llvm,zlib_archive,com_google_protobuf,com_google_protobuf_cc,png_archive,curl"
 
-if [[ "$target_platform" == linux* ]]; then
-    # do not build with MKL support
-    export TF_NEED_MKL=0
-    export BAZEL_MKL_OPT=""
+# do not build with MKL support
+export TF_NEED_MKL=0
+export BAZEL_MKL_OPT=""
 
-    mkdir -p ./bazel_output_base
-    export BAZEL_OPTS="--batch "
+mkdir -p ./bazel_output_base
+export BAZEL_OPTS="--batch "
 
-    # Linux
-    # the following arguments are useful for debugging
-    #    --logging=6
-    #    --subcommands
+# Linux
+# the following arguments are useful for debugging
+#    --logging=6
+#    --subcommands
 
-    # Set compiler and linker flags as bazel does not account for CFLAGS,
-    # CXXFLAGS and LDFLAGS.
-    BUILD_OPTS="
+# Set compiler and linker flags as bazel does not account for CFLAGS,
+# CXXFLAGS and LDFLAGS.
+BUILD_OPTS="
     --copt=-march=nocona
     --copt=-mtune=haswell
     --copt=-ftree-vectorize
@@ -48,37 +52,35 @@ if [[ "$target_platform" == linux* ]]; then
     --verbose_failures
     ${BAZEL_MKL_OPT}
     --config=opt"
-    export TF_ENABLE_XLA=1
 
-    # Python settings
-    export PYTHON_BIN_PATH=${PYTHON}
-    export PYTHON_LIB_PATH=${SP_DIR}
-    export USE_DEFAULT_PYTHON_LIB_PATH=1
+export TF_ENABLE_XLA=1
 
-    # additional settings
-    export CC_OPT_FLAGS="-march=nocona -mtune=haswell"
-    export TF_NEED_IGNITE=1
-    export TF_NEED_OPENCL=0
-    export TF_NEED_OPENCL_SYCL=0
-    export TF_NEED_CUDA=0
-    export TF_NEED_ROCM=0
-    export TF_NEED_MPI=0
-    export TF_DOWNLOAD_CLANG=0
-    export TF_SET_ANDROID_WORKSPACE=0
-    ./configure
+# Python settings
+export PYTHON_BIN_PATH=${PYTHON}
+export PYTHON_LIB_PATH=${SP_DIR}
+export USE_DEFAULT_PYTHON_LIB_PATH=1
 
-    # build using bazel
-    bazel ${BAZEL_OPTS} build ${BUILD_OPTS} //tensorflow/tools/pip_package:build_pip_package
+# additional settings
+export CC_OPT_FLAGS="-march=nocona -mtune=haswell"
+export TF_NEED_IGNITE=1
+export TF_NEED_OPENCL=0
+export TF_NEED_OPENCL_SYCL=0
+export TF_NEED_CUDA=0
+export TF_NEED_ROCM=0
+export TF_NEED_MPI=0
+export TF_DOWNLOAD_CLANG=0
+export TF_SET_ANDROID_WORKSPACE=0
+./configure
 
-    # build a whl file
-    mkdir -p $SRC_DIR/tensorflow_pkg
-    bazel-bin/tensorflow/tools/pip_package/build_pip_package $SRC_DIR/tensorflow_pkg
+# build using bazel
+bazel ${BAZEL_OPTS} build ${BUILD_OPTS} //tensorflow/tools/pip_package:build_pip_package
 
-    # install the whl using pip
-    pip install --no-deps $SRC_DIR/tensorflow_pkg/*.whl
+# build a whl file
+mkdir -p $SRC_DIR/tensorflow_pkg
+bazel-bin/tensorflow/tools/pip_package/build_pip_package $SRC_DIR/tensorflow_pkg
 
-    # The tensorboard package has the proper entrypoint
-    rm -f ${PREFIX}/bin/tensorboard
-elif [[ "$target_platform" == osx* ]]; then
-    $PYTHON -m pip install --no-deps -vv  *.whl
-fi
+# install the whl using pip
+pip install --no-deps $SRC_DIR/tensorflow_pkg/*.whl
+
+# The tensorboard package has the proper entrypoint
+rm -f ${PREFIX}/bin/tensorboard
