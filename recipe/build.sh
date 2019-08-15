1be57967f892f82a75613bad44d3cb354478aedb
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-08-15 02:57:23 -0400

    update

--- recipe/build.sh
+++ recipe/build.sh
@@ -58,10 +58,5 @@ if [[ "$target_platform" == linux* ]]; then
     bazel-bin/tensorflow/tools/pip_package/build_pip_package $SRC_DIR/tensorflow_pkg
 
     # install the whl using pip
-    pip install --no-deps $SRC_DIR/tensorflow_pkg/*.whl
-
-    # The tensorboard package has the proper entrypoint
-    rm -f ${PREFIX}/bin/tensorboard
-elif [[ "$target_platform" == osx* ]]; then
-    $PYTHON -m pip install --no-deps -vv  *.whl
+    mv $SRC_DIR/tensorflow_pkg/*.whl .
 fi
---
+++ recipe/build_pkg.bat
@@ -0,0 +1,2 @@
+# install the whl using pip
+%PYTHON% -m pip install --no-deps -vv *.whl
