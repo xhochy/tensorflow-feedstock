e98c7ef1ca46236c6c086697e7dfaa9690bb6705
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-08-15 03:38:50 -0400

    fix

--- recipe/build_pkg.bat
+++ recipe/build_pkg.bat
@@ -1,2 +1,2 @@
-# install the whl using pip
-%PYTHON% -m pip install --no-deps -vv *.whl
+pip install --no-deps -vv *.whl
+for %%f in (*.whl) do pip install --no-deps -vv "%%f"
--- recipe/build_pkg.sh
+++ recipe/build_pkg.sh
@@ -1,2 +1,3 @@
 # install the whl using pip
-$PYTHON -m pip install --no-deps -vv *.whl
+pip install --no-deps -vv *.whl
+rm ${PREFIX}/bin/tensorboard
