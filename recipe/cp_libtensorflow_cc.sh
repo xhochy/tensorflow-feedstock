11e93489ad71844348d939878517d713298cd949
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-09-10 01:03:05 -0400

    Update cp_libtensorflow_cc.sh

--- recipe/cp_libtensorflow_cc.sh
+++ recipe/cp_libtensorflow_cc.sh
@@ -1,5 +1,5 @@
 # copy libraries
-mkdir ${PREFIX}/lib
+mkdir -p ${PREFIX}/lib
 cp bazel-bin/tensorflow/libtensorflow_cc.so ${PREFIX}/lib/
 cp bazel-bin/tensorflow/libtensorflow_framework.so ${PREFIX}/lib/
 
@@ -13,10 +13,10 @@ find bazel-work/external/com_google_absl/absl -name "*.cc" -type f -delete
 # copy includes
 mkdir -p ${PREFIX}/include/tensorflow
 cp -r bazel-genfiles/* ${PREFIX}/include/
-cp -r tensorflow/cc ${PREFIX}/include/tensorflow
-cp -r tensorflow/core ${PREFIX}/include/tensorflow
-cp -r third_party ${PREFIX}/include
+cp -r tensorflow/cc/ ${PREFIX}/include/tensorflow
+cp -r tensorflow/core/ ${PREFIX}/include/tensorflow
+cp -r third_party/ ${PREFIX}/include
 
-cp -r bazel-work/external/com_google_absl/absl ${PREFIX}/include
-cp -r bazel-work/external/eigen_archive/Eigen ${PREFIX}/include
-cp -r bazel-work/external/eigen_archive/unsupported ${PREFIX}/include
+cp -r bazel-work/external/com_google_absl/absl/ ${PREFIX}/include
+cp -r bazel-work/external/eigen_archive/Eigen/ ${PREFIX}/include
+cp -r bazel-work/external/eigen_archive/unsupported/ ${PREFIX}/include
