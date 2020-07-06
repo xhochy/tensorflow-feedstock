89d9deca77da77c45af020e16a2af40cd463b6cf
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2020-07-06 20:20:04 +0100

    Fix tests

--- recipe/cp_libtensorflow.sh
+++ recipe/cp_libtensorflow.sh
@@ -1,12 +1,7 @@
 # copy libraries
 mkdir -p ${PREFIX}/lib
-cp bazel-bin/tensorflow/libtensorflow.so ${PREFIX}/lib/
-cp bazel-bin/tensorflow/libtensorflow_framework.so ${PREFIX}/lib/
+cp bazel-bin/tensorflow/*.so ${PREFIX}/lib/
 
 # copy includes
-mkdir -p ${PREFIX}/include/tensorflow
 mkdir -p ${PREFIX}/include/tensorflow/c
-mkdir -p ${PREFIX}/include/tensorflow/c/eager
-cp tensorflow/c/c_api.h ${PREFIX}/include/tensorflow/c/
-cp tensorflow/c/c_api_experimental.h ${PREFIX}/include/tensorflow/c/
-cp tensorflow/c/eager/c_api.h ${PREFIX}/include/tensorflow/c/eager/
+cp -R tensorflow/c/* ${PREFIX}/include/tensorflow/c/.
