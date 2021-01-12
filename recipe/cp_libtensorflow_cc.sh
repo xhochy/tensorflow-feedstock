65b66489feae9142362b2f9f308751b3d381e297
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-01-12 17:07:50 -0500

    update the script for libtensorflow_cc

--- recipe/cp_libtensorflow_cc.sh
+++ recipe/cp_libtensorflow_cc.sh
@@ -1,22 +1,17 @@
-# copy libraries
-mkdir -p ${PREFIX}/lib
-cp bazel-bin/tensorflow/libtensorflow_cc.so ${PREFIX}/lib/
-cp bazel-bin/tensorflow/libtensorflow_framework.so ${PREFIX}/lib/
+mkdir -p $PREFIX/lib
+# copy library
+cp -d bazel-bin/tensorflow/libtensorflow_cc.so* $PREFIX/lib/
+cp -d bazel-bin/tensorflow/libtensorflow_framework.so* $PREFIX/lib/
+cp -d $PREFIX/lib/libtensorflow_framework.so.2 $PREFIX/lib/libtensorflow_framework.so
 
-# remove cc files
-find bazel-genfiles/ -name "*.cc" -type f -delete
-find tensorflow/cc -name "*.cc" -type f -delete
-find tensorflow/core -name "*.cc" -type f -delete
-find third_party -name "*.cc" -type f -delete
-find bazel-work/external/com_google_absl/absl -name "*.cc" -type f -delete
-
-# copy includes
-mkdir -p ${PREFIX}/include/tensorflow
-cp -r bazel-genfiles/* ${PREFIX}/include/
-cp -r tensorflow/cc/ ${PREFIX}/include/tensorflow
-cp -r tensorflow/core/ ${PREFIX}/include/tensorflow
-cp -r third_party/ ${PREFIX}/include
-
-cp -r bazel-work/external/com_google_absl/absl/ ${PREFIX}/include
-cp -r bazel-work/external/eigen_archive/Eigen/ ${PREFIX}/include
-cp -r bazel-work/external/eigen_archive/unsupported/ ${PREFIX}/include
+mkdir -p $PREFIX/include
+mkdir -p $PREFIX/include/tensorflow
+# copy headers
+rsync -avzh --exclude '_virtual_includes/' --include '*/' --include '*.h' --include '*.inc' --exclude '*' bazel-bin/ $PREFIX/include/
+rsync -avzh --include '*/' --include '*.h' --include '*.inc' --exclude '*' tensorflow/cc $PREFIX/include/tensorflow/
+rsync -avzh --include '*/' --include '*.h' --include '*.inc' --exclude '*' tensorflow/core $PREFIX/include/tensorflow/
+rsync -avzh --include '*/' --include '*' --exclude '*.cc' third_party/ $PREFIX/include/third_party/
+rsync -avzh --include '*/' --include '*' --exclude '*.txt' bazel-work/external/eigen_archive/Eigen/ $PREFIX/include/Eigen/
+rsync -avzh --include '*/' --include '*' --exclude '*.txt' bazel-work/external/eigen_archive/unsupported/ $PREFIX/include/unsupported/
+rsync -avzh --include '*/' --include '*.h' --include '*.inc' --exclude '*' bazel-work/external/com_google_protobuf/src/google/ $PREFIX/include/google/
+rsync -avzh --include '*/' --include '*.h' --include '*.inc' --exclude '*' bazel-work/external/com_google_absl/absl/ $PREFIX/include/absl/
