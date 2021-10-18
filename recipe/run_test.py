4550ce3d539f017474cf998300445d0b42ebd85f
Mark Harfouche <mark.harfouche@gmail.com>
2021-10-18 15:05:57 -0400

    Expand tests

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -2,4 +2,17 @@ import tensorflow as tf
 hello = tf.constant('Hello, TensorFlow!')
 a = tf.constant(10)
 b = tf.constant(32)
-a+b
+a + b
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
