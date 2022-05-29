8a33ec43e6d8a07557716da4d2fdc4fcb0d5ec54
Uwe L. Korn <uwe.korn@quantco.com>
2022-05-29 07:24:22 +0200

    Disable half the kernels

--- recipe/build.sh
+++ recipe/build.sh
@@ -30,10 +30,10 @@ export BUILD_TARGET="
   //tensorflow/core/kernels:libtfkernel_conv_ops.so
   //tensorflow/core/kernels:libtfkernel_cwise_op.so
   //tensorflow/core/kernels:libtfkernel_unique_op.so
-  //tensorflow/core/kernels:libtfkernel_dynamic_partition_op.so
-  //tensorflow/core/kernels:libtfkernel_dynamic_stitch_op.so
-  //tensorflow/core/kernels:libtfkernel_strided_slice_op.so
 "
+#  //tensorflow/core/kernels:libtfkernel_dynamic_partition_op.so
+#  //tensorflow/core/kernels:libtfkernel_dynamic_stitch_op.so
+#  //tensorflow/core/kernels:libtfkernel_strided_slice_op.so
 
 # Python settings
 export PYTHON_BIN_PATH=${BUILD_PREFIX}/bin/python
@@ -55,9 +55,9 @@ bazel ${BAZEL_OPTS} build ${BUILD_OPTS} ${BUILD_TARGET}
 
 cp -RP bazel-bin/tensorflow/core/kernels/libtfkernel_conv_ops.so ${PREFIX}/lib/libtfkernel_conv_ops${SHLIB_EXT}
 cp -RP bazel-bin/tensorflow/core/kernels/libtfkernel_cwise_op.so ${PREFIX}/lib/libtfkernel_cwise_op${SHLIB_EXT}
-cp -RP bazel-bin/tensorflow/core/kernels/libtfkernel_unique_op.so ${PREFIX}/lib/libtfkernel_unique_op.so${SHLIB_EXT}
-cp -RP bazel-bin/tensorflow/core/kernels/libtfkernel_dynamic_partition_op.so ${PREFIX}/lib/libtfkernel_dynamic_partition_op${SHLIB_EXT}
-cp -RP bazel-bin/tensorflow/core/kernels/libtfkernel_dynamic_stitch_op.so ${PREFIX}/lib/libtfkernek_dynamic_stitch_op${SHLIB_EXT}
-cp -RP bazel-bin/tensorflow/core/kernels/libtfkernel_strided_slice_op.so ${PREFIX}/lib/libtfkernel_strided_slice_op${SHLIB_EXT}
+cp -RP bazel-bin/tensorflow/core/kernels/libtfkernel_unique_op.so ${PREFIX}/lib/libtfkernel_unique_op${SHLIB_EXT}
+# cp -RP bazel-bin/tensorflow/core/kernels/libtfkernel_dynamic_partition_op.so ${PREFIX}/lib/libtfkernel_dynamic_partition_op${SHLIB_EXT}
+# cp -RP bazel-bin/tensorflow/core/kernels/libtfkernel_dynamic_stitch_op.so ${PREFIX}/lib/libtfkernek_dynamic_stitch_op${SHLIB_EXT}
+# cp -RP bazel-bin/tensorflow/core/kernels/libtfkernel_strided_slice_op.so ${PREFIX}/lib/libtfkernel_strided_slice_op${SHLIB_EXT}
 
 bazel clean
