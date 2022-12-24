d39bcbd0b196da0e0e95a4569b5ba2295fb76a2c
Peter Williams <peter@newton.cx>
2022-12-24 09:10:06 -0500

    Cross-compilation correctness fixes
    In order to build the subpackages that have Python dependencies, they
    need to have Python and cross-python as build deps. tensorflow-estimator
    further needs to have tensorflow-base as a build dep because its build
    process needs to `import tensorflow` in order to generate API bindings,
    and in the cross-python setup the build-arch package is required to
    provide binary Python modules for import.

--- recipe/build_pkg.sh
+++ recipe/build_pkg.sh
@@ -1,5 +1,9 @@
-# install the whl using pip
-pip install --no-deps $SRC_DIR/tensorflow_pkg/*.whl
+#! /bin/bash
+
+set -exuo pipefail
+
+# install the whl making sure to use host pip/python if cross-compiling
+${PYTHON} -m pip install --no-deps $SRC_DIR/tensorflow_pkg/*.whl
 
 # The tensorboard package has the proper entrypoint
 rm -f ${PREFIX}/bin/tensorboard
