091c30eb92f9c4203c57b354c7fa34d92e1e5a53
Uwe L. Korn <uwe.korn@quantco.com>
2023-07-04 09:59:42 +0200

    Set std=c++17 in tensorflow_cc test

--- recipe/test_libtensorflow_cc.sh
+++ recipe/test_libtensorflow_cc.sh
@@ -5,5 +5,6 @@ set -exuo pipefail
 if [[ "${target_platform}" == linux-* ]]; then
   export LDFLAGS="${LDFLAGS} -lrt"
 fi
+export CXXFLAGS="${CXXFLAGS} -std=c++17"
 ${CXX} ${CXXFLAGS} ${LDFLAGS} -o test_cc test_cc.cc -ltensorflow_cc -ltensorflow_framework
 ./test_cc
