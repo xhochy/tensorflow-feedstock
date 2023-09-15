7dc53f75f77748469387262effc4d632060ad326
Uwe L. Korn <uwe.korn@quantco.com>
2023-09-15 17:35:54 +0200

    Link to absl_status

--- recipe/test_libtensorflow_cc.sh
+++ recipe/test_libtensorflow_cc.sh
@@ -6,5 +6,5 @@ if [[ "${target_platform}" == linux-* ]]; then
   export LDFLAGS="${LDFLAGS} -lrt"
 fi
 export CXXFLAGS="${CXXFLAGS} -std=c++17"
-${CXX} ${CXXFLAGS} ${LDFLAGS} -o test_cc test_cc.cc -ltensorflow_cc -ltensorflow_framework
+${CXX} ${CXXFLAGS} ${LDFLAGS} -o test_cc test_cc.cc -ltensorflow_cc -ltensorflow_framework -labsl_status
 ./test_cc
