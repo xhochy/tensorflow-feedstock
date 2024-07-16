6fd35836d36934a5910f57e2d0411c10cbf90c91
Uwe L. Korn <uwe.korn@quantco.com>
2024-07-16 20:57:48 +0200

    Fix tsl include location

--- recipe/cp_libtensorflow.sh
+++ recipe/cp_libtensorflow.sh
@@ -1,5 +1,7 @@
 # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/tools/lib_package/README.md
 tar -C ${PREFIX} -xzf $SRC_DIR/libtensorflow.tar.gz
 
+mv $PREFIX/include/external/local_tsl/tsl $PREFIX/include
+
 # Make writable so patchelf can do its magic
 chmod u+w $PREFIX/lib/libtensorflow*
